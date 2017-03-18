749e64755047418ffb867aef34a6d3c4c5302c8f
Sven Wappler <info@wappler.systems>
2017-03-18 13:38:32 +0100

    Verschönerungen

--- ext_tables.php
+++ ext_tables.php
@@ -5,25 +5,22 @@ if (!defined('TYPO3_MODE')) {
 
 if (TYPO3_MODE === 'BE') {
 
-	/**
-	 * Registers a Backend Module
-	 */
 	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'WapplerSystems.' . $_EXTKEY,
-		'tools',	 // Make module a submodule of 'tools'
-		'tx_templatemaker_m1',	// Submodule key
-		'',						// Position
-		array(
+		'WapplerSystems.Templatemaker',
+		'tools',
+		'templatemaker',
+		'',
+		[
 			'Manager' => 'intro,rename',
 			
-		),
-		array(
+		],
+		[
 			'access' => 'admin',
-			'icon'   => 'EXT:' . $_EXTKEY . '/ext_icon.gif',
-			'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_ba1.xlf',
-		)
+			'icon'   => 'EXT:templatemaker/Resources/Public/Icons/module-templatemaker.svg',
+			'labels' => 'LLL:EXT:templatemaker/Resources/Private/Language/locallang_ba1.xlf',
+		]
 	);
 
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Template Maker');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('templatemaker', 'Configuration/TypoScript', 'Template Maker');
