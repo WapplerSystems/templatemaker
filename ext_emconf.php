b002b3b28cae3dead955c2bb97c3d278b97eec52
Sven Wappler <info@wappler.systems>
2017-03-19 14:24:20 +0100

    Dependency and description update

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,19 +12,20 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Template Maker',
-	'description' => 'The template maker for the t3bootstrap.de project',
+	'description' => 'The template maker tool for the t3bootstrap.de project. You will need the t3bootstrap template.',
 	'category' => 'plugin',
 	'author' => 'Sven Wappler',
-	'author_email' => 'typo3@wappler.systems',
+	'author_email' => 'typo3YYYY@wappler.systems',
 	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.0.5',
+	'version' => '1.0.6',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-8.7.99',
+            'demotemplate' => '',
 		),
 		'conflicts' => array(
 		),
